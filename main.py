aadbe021d2177779305e572b0e938947d35f03e0
takoyaki-junior <takoyakijunior5@gmail.com>
2022-01-16 19:46:10 +0900

    20210116 2回目コミット

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"message": "Hello World"}
+
+
+@app.get("/country/{country_name}")
+async def country(country_name: str):
+    return {"country_name": country_name}
+
+
+@app.get("/country/")
+async def country(country_name: str = 'japan', country_no: int = 1):
+    return {
+        "country_name": country_name,
+        "country_no": country_no
+    }
